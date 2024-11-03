1d5003f1c342b7c396508b81e77743d1f88af4ad
vipul prajapati <vipulgithub020@gmail.com>
2024-11-03 14:46:26 +0000

    import carsousel and until

---
+++ src/main.js
@@ -0,0 +1,9 @@
+// API
+/*
+breeds/list/all - List of all breeds
+breed/Affenpinscher/images/random  - single image
+breed/Affenpinscher/images  - single images
+*/
+
+import Carousel from "./components/Carousel";
+import { capitalize } from "./until";

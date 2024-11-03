ec55b7c05d30b104ab6d1a88b6845e074d733391
vipul prajapati <vipulgithub020@gmail.com>
2024-11-03 14:47:33 +0000

    DOM Selection select & carousel-inner

--- src/main.js
+++ src/main.js
@@ -7,3 +7,7 @@ breed/Affenpinscher/images  - single images
 
 import Carousel from "./components/Carousel";
 import { capitalize } from "./until";
+
+// DOM Selection
+const selectEl = document.querySelector("select");
+const carouselContainer = document.querySelector(".carousel-inner");
